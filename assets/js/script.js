7548815e2737265782d1c9373b0aa5d90fe2a809
Nguyen Manh Hung <22092005nguyenhung@gmail.com>
2025-01-09 15:30:49 +0700

    commit step 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,53 @@ document.getElementById('scrollToTopButton').addEventListener('click', function(
         behavior: 'smooth' // Cuộn mượt lên đầu trang
     });
 });
+
+
+// Khi click vào button next step thì điều hướng sang trang mới
+
+const btnNextStep = document.querySelector('#nextStepBtn');
+if (btnNextStep) {
+    btnNextStep.addEventListener("click", () => {
+        window.location.href = "step2.html";
+    })
+}
+
+
+
+// Dùng khi ở step 2 mà chọn 1 trong các option thì sẽ tự render ra giá tri được chọn
+const selectOption = document.querySelector('#filter-select');
+selectOption.addEventListener('change', function () {
+    let selectedValue = this.value.toLowerCase();
+    let listItems = document.querySelectorAll('#company-list li');
+    listItems.forEach(function (item) {
+        if (selectedValue === 'all' || item.getAttribute('data-company') === selectedValue) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
+
+
+// Khi click vào option ở step 2
+document.querySelectorAll('#company-list li').forEach(item => {
+    item.addEventListener('click', () => {
+        // Remove active class from all items
+        document.querySelectorAll('#company-list li').forEach(li => {
+            li.classList.remove('active');
+        });
+        // Add active class to the clicked item
+        item.classList.add('active');
+    });
+});
+
+
+
+
+// trở lại trang chủ
+const buttonToHome = document.querySelector(".back-to-1");
+if (buttonToHome) {
+    buttonToHome.addEventListener("click", () => {
+        window.location.href = "index.html";
+    })
+}
